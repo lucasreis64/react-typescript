10e09e8a9b36ab8780e3a87b4b6cb38742f77e9e
Lucas <lucasreis64@gmail.com>
2022-12-17 23:51:13 -0300

    feat: dashboard component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard } from "../pages";
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/main-page" element={<p>Main Page</p>} />
+            <Route path="/main-page" element={<Dashboard/>} />
             
             <Route path="*" element={<Navigate to="/main-page" />} />
         </Routes>
